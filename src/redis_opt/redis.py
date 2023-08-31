48d0cf0612ef58ed68b15442dd8b7d7e92bf8798
wangxuefei <wang_xuefei@neusoft.com>
2023-08-31 17:37:17 +0800

    完善redis封装；

--- src/redis_opt/redis.py
+++ src/redis_opt/redis.py
@@ -4,6 +4,16 @@ class Redis:
     def __init__(self, cf):
         self.r = redis.StrictRedis(host=cf["redis"]["url"], port=cf['redis']['port'], db=0)
         self.timeout = cf['redis']['timeout']
+        self.pubsub = self.r.pubsub()
+
+    def Subscribe(self, key, callback=None):
+        self.pubsub.subscribe(key)
+        for message in self.pubsub.listen():
+            if message['type'] == 'message':
+                if callback:
+                    callback(message['data'])
+                else:
+                    return message['data']
 
     def Publish(self, key, content):
         self.r.publish(key, content) 
